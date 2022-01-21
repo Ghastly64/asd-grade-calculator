261f26b6de8ff9d65ec7a466567eed96d2065417
Mark Sorial <msorial44@gmail.com>
2022-01-22 00:54:59 +0400

    finished form and calculations

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import React from 'react';
 import logo from './logo.svg'
 import 'antd/dist/antd.css';
 import './App.scss';
-import {Form, Input, Button, Space, Select, Checkbox} from 'antd';
+import {Form, Input, InputNumber, Button, Space, Select, Checkbox} from 'antd';
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 
 const { Option } = Select;
 
 const onFinish = (values: any) => {
-  console.log('Received values of form:', values);
+  var total = 0;
+  var totalCredits = 0;
+  for (var i = 0; i < values.class_list.length; i++) {
+    var currTotal = 0;
+    currTotal += values.class_list[i].grade
+    if (values.class_list[i].isAP) {
+      currTotal += 0.25
+    }
+    totalCredits += values.class_list[i].credits
+    total += (currTotal * values.class_list[i].credits)
+  }
+  console.log("your gpa is " + (total / totalCredits))
 };
 
 function App() {
@@ -33,30 +44,37 @@ function App() {
                     </Form.Item>
                     <Form.Item
                       {...restField}
+                      name={[name, 'credits']}
+                      rules={[{ required: true, message: 'Missing credits', type: 'number' }]}
+                    >
+                      <InputNumber style={{ width: 100 }} placeholder="Credits"/>
+                    </Form.Item>
+                    <Form.Item
+                      {...restField}
                       name={[name, 'grade']}
                       rules={[{ required: true, message: 'Missing grade' }]}
                     >
                       <Select
+                        style={{ width: 100 }}
                         placeholder="Grade"
                         allowClear
                       >
-                        <Option value="A+">A+</Option>
-                        <Option value="A">A</Option>
-                        <Option value="A-">A-</Option>
-                        <Option value="B+">B+</Option>
-                        <Option value="B">B</Option>
-                        <Option value="B-">B-</Option>
-                        <Option value="C+">C+</Option>
-                        <Option value="C">C</Option>
-                        <Option value="C-">C-</Option>
-                        <Option value="D+">D+</Option>
-                        <Option value="D">D</Option>
-                        <Option value="D-">D-</Option>
-                        <Option value="F">F</Option>
+                        <Option value={4}>A</Option>
+                        <Option value={3.7}>A-</Option>
+                        <Option value={3.3}>B+</Option>
+                        <Option value={3}>B</Option>
+                        <Option value={2.7}>B-</Option>
+                        <Option value={2.3}>C+</Option>
+                        <Option value={2}>C</Option>
+                        <Option value={1.7}>C-</Option>
+                        <Option value={1.3}>D+</Option>
+                        <Option value={1}>D</Option>
+                        <Option value={0.5}>D-</Option>
+                        <Option value={0}>F</Option>
                         
                       </Select>
                     </Form.Item>
-                    <Form.Item
+                    <Form.Item valuePropName="checked"
                       {...restField}
                       name={[name, 'isAP']}
                     >
